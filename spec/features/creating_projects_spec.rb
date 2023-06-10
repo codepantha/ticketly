147c8848c71d1cd75bfcc9585103552b5248f24d
codepanther <pezechigozirim@gmail.com>
2023-06-10 19:03:23 +0100

    Add title functionality for project show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -11,5 +11,12 @@ RSpec.feature 'Users can create new projects' do
     click_button 'Create Project'
 
     expect(page).to have_content 'Project has been created.'
+
+    # verify that we are on the show page
+    project = Project.find_by!(name: 'Visual Studio Code')
+    expect(page.current_url).to eq project_url(project)
+
+    title = 'Visual Studio Code - Projects - Ticketly'
+    expect(page).to have_title title
   end
 end
