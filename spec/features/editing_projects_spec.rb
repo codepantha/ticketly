46a1d1ad70b1ae67139a5592ed5e56fa49508aaa
codepanther <pezechigozirim@gmail.com>
2023-06-11 13:48:01 +0100

    Ensure that invalid project details don't get updated

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -16,4 +16,11 @@ RSpec.feature 'Users can edit existing projects' do
     expect(page).to have_content 'Project has been updated.'
     expect(page).to have_content 'VS Code'
   end
+
+  scenario 'when providing invalid attributes' do
+    fill_in 'Name', with: ''
+    click_button 'Update Project'
+
+    expect(page).to have_content 'Project has not been updated.'
+  end
 end
