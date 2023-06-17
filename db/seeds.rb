99fb79cdae7cd3962df21212ba3a69116672e22b
codepanther <pezechigozirim@gmail.com>
2023-06-17 23:39:40 +0100

    Add admin flag to User model
    Seed the some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+unless User.exists?(email: 'admin@ticketly.com')
+  User.create!(email: 'admin@ticketly.com', password: 'password', admin: true)
+end
+
+unless User.exists?(email: 'viewer@ticketly.com')
+  User.create(email: 'viewer@ticketly.com', password: 'password')
+end
+
+['Visual Studio Code', 'Internet Explorer'].each do |name|
+  unless Project.exists?(name)
+    Project.create!(name: name, description: "A sample project about #{name}")
+  end
+end
