e2241451452647a1e2ab00692b89aaef110c509c
codepanther <pezechigozirim@gmail.com>
2023-06-19 13:48:45 +0100

    Ensure archived users cannot sign in to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,18 @@ class User < ApplicationRecord
   def archive!
     self.update(archived_at: Time.now)
   end
+
+  # Archived users are not permitted to login
+  # Let's overwrite the active_for_authentication? devise method to ensure this
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+
+  # Let's overwrite the inactive_message devise method if user is archived
+  # to ensure that the correct message why a user can't log in is displayed
+  def inactive_message
+    archived_at.nil? ? super : :archived
+  end
+
+
 end
