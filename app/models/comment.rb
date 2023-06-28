153f49da09d7b0ee8f70792911e9d5e31f58e2b7
codepanther <pezechigozirim@gmail.com>
2023-06-29 00:06:59 +0100

    Add feature to have a state assigned to tickets when comments are created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,19 @@ class Comment < ApplicationRecord
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
 
+  after_create :set_ticket_state
+
   validates :text, presence: true
 
   scope :persisted, -> { where.not(id: nil) }
   scope :ordered, -> { order(created_at: :asc) }
+
+  belongs_to :state, optional: true
+
+  private
+
+  def set_ticket_state
+    ticket.state = state
+    ticket.save!
+  end
 end
